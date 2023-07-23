719caabd8553b121e49af601787f4ecd41e9ad8d
tutu <t433567423@163.com>
2023-07-23 17:40:16 +0800

    feat: 用户注册接口

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,16 @@
 // 全局app
-import Koa from 'koa'
+const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
 
+const userRouter = new Router({ prefix: '/users' })
+
+userRouter.post('/', (ctx: any) => {
+  ctx.body = '创建用户成功'
+})
+app.use(userRouter.routes())
+app.use(userRouter.allowedMethods())
+
 module.exports = app
+export {}
