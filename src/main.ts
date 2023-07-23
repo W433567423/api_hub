c7d828c02b62da1190cb9467650b12cb2f6c254c
tutu <t433567423@163.com>
2023-07-23 16:20:52 +0800

    init: eslint

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-const msg: string = 'Hello World';
-console.log(msg)
+import Koa from 'koa'
+
+const app = new Koa()
+const msg: string = 'Hello World'
+
+app.use(async (ctx: Koa.Context): Promise<void> => {
+  ctx.body = msg
+})
+
+app.listen(7000, () => {
+  console.log('服务器已开启')
+})
