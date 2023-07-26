e6832de20f30689b4f2b885cb2345cfdefc5bbf9
tutu <t433567423@163.com>
2023-07-26 14:26:50 +0800

    feat: 路由整理

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,19 +1,15 @@
 // 全局app
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
-const userRouter = require('../router/user.router')
-const authRouter = require('../router/auth.router')
+const useRouters = require('../router')
 const { errorHandle } = require('./error-handle')
 
 const app = new Koa()
 
 app.use(bodyParser())
 
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-
-app.use(authRouter.routes())
-app.use(authRouter.allowedMethods())
+// 路由注册
+useRouters(app)
 
 app.on('error', errorHandle)
 
