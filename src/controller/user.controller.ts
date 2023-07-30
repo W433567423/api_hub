a8387fda2272b64997e02d685450a02f36a7281d
tutu <t433567423@163.com>
2023-07-30 22:12:07 +0800

    feat: 参数类型检测

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,17 +1,19 @@
+import { type ILoginParams } from '../service/type'
+
 const service = require('../service/user.service')
 
 class UserController {
   /**
-   * DONE
-   * @description: 事件: 用户-注册
-   * @params: {}
-   * @return: undefined
-   * @author: tutu
-   * @time: 2023/7/23 17:48
-   */
+     * DONE
+     * @description: 事件: 用户-注册
+     * @params: {}
+     * @return: undefined
+     * @author: tutu
+     * @time: 2023/7/23 17:48
+     */
   async registry (ctx: any): Promise<void> {
     // 获取用户请求得到参数
-    const user = ctx.request.body
+    const user: ILoginParams = ctx.request.body
 
     // 将用户保存到数据库
     await service.create(user)
@@ -20,7 +22,7 @@ class UserController {
     ctx.body = { msg: '注册成功' }
   }
 
-  async test (ctx) {
+  async test (ctx: any) {
     ctx.body = { success: 'ok' }
   }
 }
