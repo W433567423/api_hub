381b29c5728b902c12846a6f9b4ba4b429a012c8
tutu <t433567423@163.com>
2023-07-30 22:19:51 +0800

    feat: 参数类型检测

--- src/controller/comment.controller.ts
+++ src/controller/comment.controller.ts
@@ -1,24 +1,26 @@
+import { type IUser } from '../service/type'
+
 const errorType = require('../constants/error-types')
 const CommentService = require('../service/comment.service')
 
 class CommentController {
   /**
-   * DONE
-   * @description: 事件: 发布comment
-   * @params: {}
-   * @return: undefined
-   * @author: tutu
-   * @time: 2023/7/27 15:50
-   */
-  async publish (ctx) {
-    const { momentId, content } = ctx.request.body
-    const { id } = ctx.user
+     * DONE
+     * @description: 事件: 发布comment
+     * @params: {}
+     * @return: undefined
+     * @author: tutu
+     * @time: 2023/7/27 15:50
+     */
+  async publish (ctx: any) {
+    const { momentId, content }: { momentId: number | undefined, content: string | undefined } = ctx.request.body
+    const { id } = ctx.user as IUser
     if (!momentId || !content) {
       const error = new Error(errorType.NO_PARAMS)
       ctx.app.emit('error', error, ctx)
     }
     try {
-      const dbRes = await CommentService.insertComment(id, momentId, content)
+      const dbRes = await CommentService.insertComment(id, momentId as number, content as string)
       // 获取用户数据
       ctx.body = { msg: '发表评论成功', data: dbRes }
     } catch (e) {
@@ -28,22 +30,22 @@ class CommentController {
   }
 
   /**
-   * DONE
-   * @description: 事件: 回复conment的comment 发布
-   * @params: {}
-   * @return: undefined
-   * @author: tutu
-   * @time: 2023/7/28 10:05
-   */
-  async reply (ctx) {
-    const { momentId, commentId, content } = ctx.request.body
-    const { id } = ctx.user
+     * DONE
+     * @description: 事件: 回复comment的comment 发布
+     * @params: {}
+     * @return: undefined
+     * @author: tutu
+     * @time: 2023/7/28 10:05
+     */
+  async reply (ctx: any) {
+    const { momentId, commentId, content }: { momentId: number | undefined, content: string | undefined, commentId: number | undefined } = ctx.request.body
+    const { id } = ctx.user as IUser
     if (!momentId || !content) {
       const error = new Error(errorType.NO_PARAMS)
       ctx.app.emit('error', error, ctx)
     }
     try {
-      const dbRes = await CommentService.insertComment(id, momentId, content, commentId)
+      const dbRes = await CommentService.insertComment(id, momentId as number, content as string, commentId as number)
       // 获取用户数据
       ctx.body = { msg: '回复评论成功', data: dbRes }
     } catch (e) {
@@ -53,22 +55,22 @@ class CommentController {
   }
 
   /**
-   * DONE
-   * @description: 事件: 修改comment by id
-   * @params: {}
-   * @return: undefined
-   * @author: tutu
-   * @time: 2023/7/28 10:21
-   */
-  async changeCommentById (ctx) {
-    const { commentId } = ctx.params
-    const { content } = ctx.request.body
+     * DONE
+     * @description: 事件: 修改comment by id
+     * @params: {}
+     * @return: undefined
+     * @author: tutu
+     * @time: 2023/7/28 10:21
+     */
+  async changeCommentById (ctx: any) {
+    const { commentId }: { commentId: number | undefined } = ctx.params
+    const { content }: { content: string | undefined } = ctx.request.body
     if (!content) {
       const error = new Error(errorType.NO_PARAMS)
       ctx.app.emit('error', error, ctx)
     }
     try {
-      const dbRes = await CommentService.changeCommentbyId(commentId, content)
+      const dbRes = await CommentService.changeCommentbyId(commentId as number, content as string)
       // 获取用户数据
       ctx.body = { msg: '修改comment成功', data: dbRes }
     } catch (e) {
@@ -78,17 +80,17 @@ class CommentController {
   }
 
   /**
-   * DONE
-   * @description: 事件: 用户删除其comment by id
-   * @params: {}
-   * @return: undefined
-   * @author: tutu
-   * @time: 2023/7/28 10:54
-   */
-  async deleteCommentById (ctx) {
-    const { commentId } = ctx.params
+     * DONE
+     * @description: 事件: 用户删除其comment by id
+     * @params: {}
+     * @return: undefined
+     * @author: tutu
+     * @time: 2023/7/28 10:54
+     */
+  async deleteCommentById (ctx: any) {
+    const { commentId }: { commentId: number | undefined } = ctx.params
     try {
-      const dbRes = await CommentService.delCommentById(commentId)
+      const dbRes = await CommentService.delCommentById(commentId as number)
       // 获取用户数据
       ctx.body = { msg: '删除comment成功', data: dbRes }
     } catch (e) {
@@ -98,20 +100,20 @@ class CommentController {
   }
 
   /**
-   * DONE
-   * @description: 事件: 获取Comments by id
-   * @params: {}
-   * @return: undefined
-   * @author: tutu
-   * @time: 2023/7/28 16:14
-   */
-  async getCommentsById (ctx) {
-    const { momentId } = ctx.params
+     * DONE
+     * @description: 事件: 获取Comments by id
+     * @params: {}
+     * @return: undefined
+     * @author: tutu
+     * @time: 2023/7/28 16:14
+     */
+  async getCommentsById (ctx: any) {
+    const { momentId }: { momentId: number | undefined } = ctx.params
     if (!momentId) {
       const error = new Error(errorType.NO_PARAMS)
       ctx.app.emit('error', error, ctx)
     }
-    ctx.body = { msg: '查询comments成功', data: await CommentService.getCommentsByMomentId(momentId) }
+    ctx.body = { msg: '查询comments成功', data: await CommentService.getCommentsByMomentId(momentId as number) }
   }
 }
 
--- src/service/comment.service.ts
+++ src/service/comment.service.ts
@@ -2,7 +2,7 @@ const db = require('../app/database')
 
 class CommentService {
   // 插入数据到数据库
-  async insertComment (userId: string, momentId: string, content: string, commentId?: string) {
+  async insertComment (userId: number, momentId: number, content: string, commentId?: number) {
     if (commentId) {
       const sqlString = 'INSERT INTO comment (user_id,moment_id,comment_id,content) VALUES( ?, ?, ?, ?);'
       return (await db.execute(sqlString, [userId, momentId, commentId, content]))[0]
@@ -15,16 +15,16 @@ class CommentService {
   // 修改comment by id and userId
   async getCommentByIdAndUserId (userId: string, commentId: string) {
     const sqlString = `SELECT *
-                       FROM comment
-                       WHERE comment.id = ?
-                         AND comment.user_id = ?;
-    `
+                           FROM comment
+                           WHERE comment.id = ?
+                             AND comment.user_id = ?;
+        `
     const dbRes = await db.execute(sqlString, [commentId, userId])
     return !!dbRes[0][0]
   }
 
   // 修改comment by id
-  async changeCommentbyId (commentId: string, content: string) {
+  async changeCommentbyId (commentId: number, content: string) {
     console.log(content, commentId)
     const sqlString = 'UPDATE  comment SET content = ? WHERE id = ?;'
     return (await db.execute(sqlString, [content, commentId]))[0]
@@ -33,24 +33,24 @@ class CommentService {
   // 删除comment by id
   async delCommentById (commentId: number) {
     const sqlString = `DELETE
-                       FROM comment
-                       WHERE comment.id = ?;
-    `
+                           FROM comment
+                           WHERE comment.id = ?;
+        `
     await db.execute(sqlString, [commentId])
   }
 
   // 获得comments by momentId
-  async getCommentsByMomentId (momentId: string) {
+  async getCommentsByMomentId (momentId: number) {
     const sqlString = `
-      SELECT c.id,
-             c.content,
-             c.comment_id,
-             c.createAt                                  createTime,
-             JSON_OBJECT('id', u.id, 'name', u.username) publishAuthor
-      FROM comment c
-             LEFT JOIN user u ON u.id = c.user_id
-      WHERE c.moment_id = ?;
-    `
+            SELECT c.id,
+                   c.content,
+                   c.comment_id,
+                   c.createAt                                  createTime,
+                   JSON_OBJECT('id', u.id, 'name', u.username) publishAuthor
+            FROM comment c
+                     LEFT JOIN user u ON u.id = c.user_id
+            WHERE c.moment_id = ?;
+        `
     return (await db.execute(sqlString, [momentId]))[0]
   }
 }
