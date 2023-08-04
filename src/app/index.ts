2880d3f2059fa82d29475954a048d65f641d0518
tutu <t433567423@163.com>
2023-08-04 08:05:18 +0800

    feat: 新增swagger

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,11 @@
 // 全局app
+import { koaSwagger } from 'koa2-swagger-ui'
+
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const useRouters = require('../router')
 const { errorHandle } = require('./error-handle')
+const swagger = require('./swagger')
 
 const app = new Koa()
 
@@ -12,6 +15,16 @@ app.use(bodyParser())
 useRouters(app)
 
 app.on('error', errorHandle)
+
+app.use(swagger.routes(), swagger.allowedMethods())
+app.use(
+  koaSwagger({
+    routePrefix: '/swagger', // host at /swagger instead of default /docs
+    swaggerOptions: {
+      url: '/swagger.json' // example path to json
+    }
+  })
+)
 
 module.exports = app
 export {}
