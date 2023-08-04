9907b32e4fb225d4fc2c343c0f7750f059d7ccd5
tutu <t433567423@163.com>
2023-08-04 10:20:55 +0800

    feat: 新增swagger

--- src/app/swagger.ts
+++ src/app/swagger.ts
@@ -1,55 +1,35 @@
+const path = require('path')
 const router = require('koa-router')() // 引入路由函数
-// const path = require('path')
 const swaggerJSDoc = require('swagger-jsdoc')
+
 const swaggerDefinition = {
   info: {
-    description:
-      '本项目基于js，实现了一个几乎完整的api系统，包含了用户系统、说说(心情)系统、评论系统、标签系统、文件系统五个部分',
-    version: '1.0.0',
     title: 'hub Swagger',
-    // 服务条款
-    // termsOfService: 'http://swagger.io/terms/',
+    summary: 'a swagger API for hub',
+    description: 'This is a swagger API for hub',
+    termsOfService: 'https://example.com/terms/',
     contact: {
-      name: 'Contact developers',
-      url: 'https://mail.qq.com/',
+      name: 'tutu',
+      url: 'https://wtututu.top',
       email: 't433567423@163.com'
     },
-    // 开源协议
     license: {
       name: 'Apache 2.0',
-      url: 'http://www.apache.org/licenses/LICENSE-2.0.html'
-    }
+      url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+    },
+    version: '1.0.1'
   },
   host: 'localhost:8001',
-  basePath: '/', // Base path (optional), host/basePath
-  schemes: ['http', 'https'],
-  securityDefinitions: {
-    server_auth: {
-      type: 'oauth2',
-      description: '登录账号密码鉴权',
-      tokenUrl: 'http://localhost:4000/image/oauth',
-      flow: 'password',
-      scopes: {
-        token: 'modify pets in your account'
-      }
-    },
-    token: {
-      type: 'apiKey',
-      name: 'token',
-      in: 'header'
-    }
-  }
+  basePath: '/' // Base path (optional)
 }
 const options = {
   swaggerDefinition,
-  // 写有注解的router的存放地址(当你新增swagger时文档里没显示出来的话那么就是这边地址没有加进去)
-  apis: ['./routes/*.js', './routes/image/*.js'] // routes下所有的js文件和routes/image下所有js文件
+  apis: [path.join(__dirname, '../controller/*.ts')] // 写有注解的router的存放地址, 最好path.join()
 }
 const swaggerSpec = swaggerJSDoc(options)
 // 通过路由获取生成的注解文件
-router.get('/swagger.json', async ctx => {
+router.get('/swagger.json', async function (ctx) {
   ctx.set('Content-Type', 'application/json')
   ctx.body = swaggerSpec
 })
-
 module.exports = router
